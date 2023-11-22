47b393e45cfa544a0e3dadcc2737f327abcf6b47
Rahul-Kumar-Paswan <rahulkumarpaswan941@gmail.com>
2023-11-22 12:01:53 +0530

    debugg

--- variables.tf
+++ variables.tf
@@ -9,8 +9,8 @@ variable "instance_type" {}
 variable "instance_name" {}
 variable "private_key_path" {}
 variable "public_key_path" {}
-variable "access_key" {}
-variable "secret_key" {}
+# variable "access_key" {}
+# variable "secret_key" {}
 variable "region" {}
 
 
